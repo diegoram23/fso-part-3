880bdee08eae93a9d967f3856f1b223a35520442
diegoram23 <diego.pta.oc@gmail.com>
2023-03-06 11:07:58 -0800

    completed exercise 3.5

--- index.js
+++ index.js
@@ -45,6 +45,30 @@ app.get('/api/persons/:id', (request, response) => {
         ? response.json(person) : response.status(404).end()
 })
 
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(person => person.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'Name missing'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => id !== person.id)
