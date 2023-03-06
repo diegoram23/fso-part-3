2e1ab2e73e8d71aae382fadb88c05df56f5fb824
diegoram23 <diego.pta.oc@gmail.com>
2023-03-06 10:54:45 -0800

    completed exercise 3.3

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ app.use(express.json())
 
 let persons = [
     {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
     },
     {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
     },
     {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
     },
     {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
     }
 ]
 
@@ -34,6 +34,15 @@ app.get('/api/persons', (request, response) => {
 app.get('/info', (request, response) => {
     response.send(`<p> Phonebook has info for ${persons.length} people </p>
     <p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id);
+    const person = persons.find(person => id === person.id)
+    console.log(person)
+    person
+        ? response.json(person) : response.status(404).end()
 })
 
 // app.get('/api/notes/:id', (request, response) => {
